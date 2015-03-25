d744325f2158e2af073b2cf4dca26393f5691925
Brendan Abbott <brendan@bloodbone.ws>
2015-03-25 20:15:03 +1000

    Update AWS lib, CS fixes

--- lib/class.s3Facade.php
+++ lib/class.s3Facade.php
@@ -7,48 +7,48 @@ use Guzzle\Http\EntityBody;
 
 if (!defined('__IN_SYMPHONY__')) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
 
-class S3Facade {
+class S3Facade
+{
+    /**
+     * @var S3Client
+     */
+    private $s3;
 
-	private $s3;
-	public function __construct($accessKey = null, $secretKey = null) {
-		
-		$this->s3 = S3Client::factory(array(
-			'key' => $accessKey,
-			'secret' => $secretKey
-		));
-	}
+    public function __construct($accessKey = null, $secretKey = null)
+    {
+        $this->s3 = S3Client::factory(array(
+            'key' => $accessKey,
+            'secret' => $secretKey
+        ));
+    }
 
-	public function listBuckets() {
+    public function doesBucketExist($bucket)
+    {
+        return $this->s3->doesBucketExist($bucket);
+    }
 
-		$result = $this->s3->listBuckets();
+    public function listBuckets()
+    {
+        $result = $this->s3->listBuckets();
 
-		return $result['Buckets'];
-	}
+        return $result['Buckets'];
+    }
 
-	public function deleteObject($bucket, $key) {
+    public function putObject($bucket, $key, $filePath, $options)
+    {
+        $options['Bucket'] = $bucket;
+        $options['Key'] = $key;
+        $options['Body'] = EntityBody::factory(fopen($filePath, 'r+'));
 
-		return $this->s3->deleteObject(array(
-			'Bucket' => $bucket,
-			'Key' => $key
-		)); 
-	}
+        return $this->s3->putObject($options);
+    }
 
-	/**
-	 * 
-	 */
-	public function putObject($bucket, $key, $filePath, $options) {
-
-		$options['Bucket'] = $bucket;
-		$options['Key'] = $key;
-		$options['Body'] = EntityBody::factory(fopen($filePath, 'r+'));
-
-		return $this->s3->putObject($options);
-	}
-
-	public function doesBucketExist($bucket) {
-		
-		return $this->s3->doesBucketExist($bucket);
-	}
-
-
+    public function deleteObject($bucket, $key)
+    {
+        return $this->s3->deleteObject(array(
+            'Bucket' => $bucket,
+            'Key' => $key
+        ));
+    }
 }
+
